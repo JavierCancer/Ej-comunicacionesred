c27e63baa0adf621489eaad9311b56263eb6c151
Javier <Javier@Javi>
2014-10-09 20:29:19 +0200

    commit comunicaciones en red

---
+++ README.md
@@ -0,0 +1,2 @@
+Cada ejercicio con sus correspondientes clases, en el propio nombre ya indica a cual pertenece.
+Hay que tener en cuenta que la parte de cliente, en hostname hay que poner la ip de la maquina donde se ejecute.
---
+++ src/main/java/edu/upc/eetac/dsa/jcancer/ejercicios_comunicacionesred/Ej12quehoraesKnockKnockServer.java
@@ -0,0 +1,41 @@
+package edu.upc.eetac.dsa.jcancer.ejercicios_comunicacionesred;
+
+import java.net.*;
+import java.io.*;
+
+public class Ej12quehoraesKnockKnockServer {
+    public static void main(String[] args) throws IOException {
+       
+        
+
+        int portNumber = 2008;
+
+        try ( 
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+        
+            String inputLine, outputLine;
+            
+            // Initiate conversation with client
+            Ej12KnockKnockProtocol kkp = new Ej12KnockKnockProtocol();
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye."))
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
